f18f95fb1d38955a7e56a0b1745cd6ed00057923
Asjadgit <naveedasjad840@gmail.com>
2026-04-24 17:00:43 +0500

    fix(ci): fix tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,8 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::paginate(10);
-        // return view('users.index', compact('users'));
-        return 'Index View';
+        return view('users.index', compact('users'));
     }
 
     /**
